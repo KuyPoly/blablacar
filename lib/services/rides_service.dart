1f0ec6c4d7c95b6e89f24fa361aaf543915f900b
KuyPoly <kuypoly297@gmail.com>
2026-02-06 09:58:24 +0700

    BLA-001 Filter Ride

--- lib/services/rides_service.dart
+++ lib/services/rides_service.dart
@@ -1,12 +1,48 @@
 import '../data/dummy_data.dart';
+import '../model/ride/locations.dart';
 import '../model/ride/ride.dart';
 
-////
-///   This service handles:
-///   - The list of available rides
-///
 class RidesService {
-  static List<Ride> allRides = fakeRides;
+  static List<Ride> availableRides = fakeRides;
 
+  static get allRides => availableRides; // TODO for now fake data
 
+  //
+  //  filter the rides starting from given departure location
+  //
+  static List<Ride> filterByDeparture(Location departure) {
+    List<Ride> filteredRide = [];
+    allRides.map((e) {
+      if (e.departureLocation == departure) {
+        filteredRide.add(e);
+      }
+    });
+    return filteredRide;
+  }
+
+  //
+  //  filter the rides starting for the given requested seat number
+  //
+  static List<Ride> filterBySeatRequested(int requestedSeat) {
+    List<Ride> filterSeat = [];
+    allRides.map((e) {
+      if (e.remainingSeats >=  requestedSeat) {
+        filterSeat.add(e);
+      }
+    });
+    return filterSeat;
+  }
+
+  //
+  //  filter the rides   with several optional criteria (flexible filter options)
+  //
+  static List<Ride> filterBy({Location? departure, int? seatRequested}) {
+    List<Ride> filterBy = [];
+    allRides.map((e) {
+      if () {
+        filterBy.add(e);
+      }
+    });
+    return [];
+  }
 }
