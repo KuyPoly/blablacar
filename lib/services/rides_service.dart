1454ef6c5219e1615c5dec6eafe109c24f2ec8de
KuyPoly <kuypoly297@gmail.com>
2026-02-06 10:20:14 +0700

    correction

--- lib/services/rides_service.dart
+++ lib/services/rides_service.dart
@@ -3,46 +3,38 @@ import '../model/ride/locations.dart';
 import '../model/ride/ride.dart';
 
 class RidesService {
-  static List<Ride> availableRides = fakeRides;
-
-  static get allRides => availableRides; // TODO for now fake data
+  static List<Ride> availableRides = fakeRides; // TODO for now fake data
 
   //
   //  filter the rides starting from given departure location
   //
-  static List<Ride> filterByDeparture(Location departure) {
-    List<Ride> filteredRide = [];
-    allRides.map((e) {
-      if (e.departureLocation == departure) {
-        filteredRide.add(e);
-      }
-    });
-    return filteredRide;
+  static List<Ride> _filterByDeparture(List<Ride> rides, Location departure) {
+    return rides.where((ride) => ride.departureLocation == departure).toList();
   }
 
   //
   //  filter the rides starting for the given requested seat number
   //
-  static List<Ride> filterBySeatRequested(int requestedSeat) {
-    List<Ride> filterSeat = [];
-    allRides.map((e) {
-      if (e.remainingSeats >=  requestedSeat) {
-        filterSeat.add(e);
-      }
-    });
-    return filterSeat;
+  static List<Ride> _filterBySeatRequested(
+    List<Ride> rides,
+    int requestedSeat,
+  ) {
+    return rides.where((ride) => ride.availableSeats >= requestedSeat).toList();
   }
 
   //
   //  filter the rides   with several optional criteria (flexible filter options)
   //
   static List<Ride> filterBy({Location? departure, int? seatRequested}) {
-    List<Ride> filterBy = [];
-    allRides.map((e) {
-      if () {
-        filterBy.add(e);
-      }
-    });
-    return [];
+    List<Ride> result = availableRides;
+
+    if (departure != null) {
+      result = _filterByDeparture(result, departure);
+    }
+    if (seatRequested != null) {
+      result = _filterBySeatRequested(result, seatRequested);
+    }
+
+    return result;
   }
 }
